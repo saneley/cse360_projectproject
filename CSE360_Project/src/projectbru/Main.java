9a38e4c68af99a1c7efecdb024c2a5937341682b
walnu <walnu@AndrewLaptop>
2024-04-07 19:07:30 -0700

    Updated PatientView

---
+++ CSE360_Project/src/projectbru/Main.java
@@ -0,0 +1,52 @@
+package projectbru;
+
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.*;
+import javafx.scene.paint.Color;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Orientation;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class Main extends Application {
+	 
+	 @Override
+	 public void start(Stage stage) {
+	  try {
+	   
+	   Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+	   Scene scene = new Scene(root);
+	   stage.setScene(scene);
+	   stage.show();
+	   
+	  } catch(Exception e) {
+	   e.printStackTrace();
+	  }
+	 } 
+
+	 public static void main(String[] args) {
+	  launch(args);
+	 }
+	}
---
+++ CSE360_Project/src/projectbru/SceneController.java
@@ -0,0 +1,87 @@
+package projectbru;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class SceneController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	@FXML
+	private Button loginPersonnel;
+	@FXML
+	private Button loginUser;
+	@FXML
+	private TextField username;
+	@FXML
+	private TextField password;
+	@FXML
+	private Label loginConsole;
+	
+	public void switchToScene1(ActionEvent event) throws IOException {
+		
+		//if (username.getText().toString().equals("nurse") && password.getText().toString().equals("awesomesauce"))
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+			
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		//checkLogin();
+	}
+	public void switchToScene2(ActionEvent event) throws IOException {
+		//Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+		if (username.getText().toString().equals("nurse") && password.getText().toString().equals("awesomesauce123"))
+		{
+			root = FXMLLoader.load(getClass().getResource("NurseView.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		else
+		{
+			loginConsole.setText("Invalid Personnel Login");
+		}
+	}
+	public void switchToScene3(ActionEvent event) throws IOException {
+		if (username.getText().toString().equals("PatientZero") && password.getText().toString().equals("epicsauce123"))
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("PatientView.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		else
+		{
+			loginConsole.setText("Invalid Patient Login");
+		}
+	}
+	private void checkLogin() throws IOException {
+		//Main m = new Main();
+		/*if (username.getText().toString().equals("nurse") && password.getText().toString().equals("awesomesauce"))
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		*/
+	}
+
+}
